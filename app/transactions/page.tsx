306b309cda0209dee03972a9048780e468e2d61f
soojeen <soojeen@gmail.com>
2023-04-10 23:29:27 -0400

    working edgedb

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,7 +1,40 @@
 import { createClient } from 'edgedb'
 
-export const client = createClient()
+const client = createClient()
 
-export default function Page() {
-  return <div>TESTING</div>
+type Transaction = {
+  id: string
+  transaction_date: string
+  amount: number
+  name: string
+  transaction_type: string
+}
+
+const query = `select Transaction {
+  id,
+  transaction_date,
+  amount,
+  name,
+  transaction_type
+};
+`
+
+export default async function Page() {
+  const result = await client.query<Transaction>(query)
+
+  console.log(result)
+
+  return (
+    <div>
+      {result.map((tx) => (
+        <div key={tx.id}>
+          <div>{tx.id}</div>
+          <div>{tx.transaction_date}</div>
+          <div>{tx.amount}</div>
+          <div>{tx.name}</div>
+          <div>{tx.transaction_type}</div>
+        </div>
+      ))}
+    </div>
+  )
 }
