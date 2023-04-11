4b357a368ce2200267c54da91499d9b0b50337ec
soojeen <soojeen@gmail.com>
2023-04-10 22:59:28 -0400

    prettier

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,7 +1,7 @@
-import {createClient} from 'edgedb';
+import { createClient } from 'edgedb'
 
-export const client = createClient();
+export const client = createClient()
 
 export default function Page() {
-    return <div>TESTING</div>
+  return <div>TESTING</div>
 }
